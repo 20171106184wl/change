4e1d5e97b813f6df640069a30127b6904b7d93de
20171106184wl <43473327+20171106184wl@users.noreply.github.com>
2019-06-15 23:09:52 +0800

    改变

--- src/imnu/DrawLine.java
+++ src/imnu/DrawLine.java
@@ -2,16 +2,24 @@ package imnu;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 public class DrawLine extends JPanel {
-public static void main(String[] args) {
-JFrame frame = new JFrame("DrawLine");
-frame.getContentPane().add(new DrawLine());
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-frame.setSize(300, 200);
-frame.setVisible(true);
-}
-protected void paintComponent(Graphics g) {
-super.paintComponent(g);
-g.drawLine(50, 50, 200, 250);
-}
+	public static void main(String[] args) {
+	JFrame frame = new JFrame("DrawLine");
+	frame.getContentPane().add(new DrawLine());
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(300, 200);
+	frame.setVisible(true);
+	}
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		this.transpot();
+		g.drawLine(10, 10, 100, 250);
+	}
+	public List <Position> transpot(){
+		List<Position>point=new ArrayList<Position>();
+		return point;
+	}
+	
 }
---
+++ src/imnu/Position.java
@@ -0,0 +1,12 @@
+package imnu;
+
+public class Position {
+	public int x;
+	public int y;
+	public void setX(int x) {
+		this.x = 20;
+	}
+	public void setY(int y) {
+		this.y = 20;
+	}
+}
